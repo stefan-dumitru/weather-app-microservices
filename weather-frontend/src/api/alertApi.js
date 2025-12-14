4acc730046954ea1e5a24b1f82b05923ab4890fe
stefan-dumitru <stefan.dumitru785@gmail.com>
2025-12-15 00:51:20 +0200

    this could be it

--- weather-frontend/src/api/alertApi.js
+++ weather-frontend/src/api/alertApi.js
@@ -1,8 +1,8 @@
 import axiosClient from "./axiosClient";
 
 const alertApi = {
-  getAlerts: () => axiosClient.get("/alerts"),
-  addAlert: (data) => axiosClient.post("/alerts", data),
+  getAlerts: () => axiosClient.get("/alerts/"),
+  addAlert: (data) => axiosClient.post("/alerts/", data),
 };
 
 export default alertApi;
